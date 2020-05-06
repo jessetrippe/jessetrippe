74517f47a65fa3056ec7434d9b07ae1a55fe8fcb
Jesse Trippe <jesse@jessetrippe.com>
2020-05-06 14:29:35 -0700

    Trying to checkin _site folder to fix purge

--- _includes/tailwind.config.js
+++ _includes/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
-    content: ["_site/**/*.html"],
-    css: ["_site/styles/main.css"]
+    content: ["./_site/**/*.html"],
+    css: ["./_site/**/main.css"]
   },
   theme: {
     fontFamily: {

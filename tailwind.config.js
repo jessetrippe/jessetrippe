4603d06492c068cb816b79a6467cc967204a889d
Jesse Trippe <jesse@jessetrippe.com>
2021-02-23 08:41:15 -0800

    Fixing some accessibility issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,23 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: {
-    content: ["./_site/**/*.html"],
-    css: ["./_site/**/main.css"]
-  },
-  theme: {
-    fontFamily: {
-      'windsor': ['Windsor', 'Georgia', 'serif'],
-      'robotoMono': ['Roboto Mono', 'Courier', 'monospace'] 
-    },
-    extend: {
-      colors: {
-        cyan: colors.cyan,
-        warmGray: colors.warmGray,
-        lightBlue: colors.lightBlue,
-        teal: colors.teal,
-        gray: colors.gray,
-      }
-    }
-  }
+	purge: {
+		content: ["./_site/**/*.html"],
+		css: ["./_site/**/main.css"]
+	},
+	theme: {
+		fontFamily: {
+			'windsor': ['Windsor', 'Georgia', 'serif'],
+			'robotoMono': ['Roboto Mono', 'Courier', 'monospace'] 
+		},
+		extend: {
+			colors: {
+				cyan: colors.cyan,
+				warmGray: colors.warmGray,
+				lightBlue: colors.lightBlue,
+				teal: colors.teal,
+				gray: colors.gray,
+			}
+		}
+	}
 }
